4bc1e07ade078e4ce2034ada577488fc21b58917
Venkadesh <training@tiliconveli.com>
2017-08-17 17:40:31 +0530

    17.8.17 changes

--- Phptravels/src/hotels/MonthSelect.java
+++ Phptravels/src/hotels/MonthSelect.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public class MonthSelect {
@@ -22,13 +24,16 @@ public class MonthSelect {
 		driver.get("http://www.phptravels.net");
 		//driver.findElement(By.xpath("html/body/nav[1]/div/div/div/ul/li[1]/a")).click();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.cssSelector(".select2-chosen")).click();
-		driver.findElement(By.cssSelector(".select2-input.select2-focused")).sendKeys("Dub");
-		Thread.sleep(2000);
-		driver.findElement(By.xpath(".//*[@id='select2-drop']/ul/li/ul/li[1]/div")).click();
-		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath(".//*[@id='s2id_autogen3']//a")).click();
+		driver.findElement(By.xpath(".//*[@id='select2-drop']/div/input")).sendKeys("Dub");
+		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+		WebElement E= driver.findElement(By.xpath(".//*[@id='select2-drop']/ul/li/ul/li[1]/div"));
+		E.click();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElement(By.cssSelector(".form.input-lg.dpd1")).click();
-		Thread.sleep(2000);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		String ExpMonth = "November 2017";
 		driver.findElement(By.xpath("html/body/div[13]/div[1]/table/thead/tr[1]/th[3]")).click();
 		String CurrMonth=driver.findElement(By.xpath("html/body/div[13]/div[1]/table/thead/tr[1]/th[2]")).getText();
@@ -38,7 +43,7 @@ public class MonthSelect {
 		else {
 			for(int i=1;i<12;i++) {
 				driver.findElement(By.xpath("html/body/div[13]/div[1]/table/thead/tr[1]/th[3]")).click();
-				Thread.sleep(2000);
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 				CurrMonth=driver.findElement(By.xpath("html/body/div[13]/div[1]/table/thead/tr[1]/th[2]")).getText();
 				if (ExpMonth.equals(CurrMonth)) {
 					System.out.println("Month Selected"); 
@@ -46,12 +51,12 @@ public class MonthSelect {
 			}
 		}
 	}
-		Thread.sleep(2000);
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		WebElement datepicker = driver.findElement(By.xpath("html/body/div[13]/div[1]/table/tbody/tr[3]/td[3]"));
 		datepicker.click();
-		Thread.sleep(2000);	
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);	
 		driver.findElement(By.cssSelector(".form.input-lg.dpd2")).clear();
-		Thread.sleep(2000);
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		String ExpsMonth = "December 2017";
 		driver.findElement(By.xpath("html/body/div[14]/div[1]/table/thead/tr[1]/th[3]")).click();
 		String CurrsMonth=driver.findElement(By.xpath("html/body/div[14]/div[1]/table/thead/tr[1]/th[2]")).getText();
@@ -61,7 +66,7 @@ public class MonthSelect {
 		else {
 		for(int i=1;i<12;i++) {
 			driver.findElement(By.xpath("html/body/div[14]/div[1]/table/thead/tr[1]/th[3]")).click();
-			Thread.sleep(2000);
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			CurrsMonth=driver.findElement(By.xpath("html/body/div[14]/div[1]/table/thead/tr[1]/th[2]")).getText();
 			if (ExpsMonth.equals(CurrsMonth)) {
 				System.out.println("Month Selected"); 
@@ -69,21 +74,24 @@ public class MonthSelect {
 		}
 	} 
 }
-	Thread.sleep(2000);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	WebElement datepicker1 = driver.findElement(By.xpath("html/body/div[14]/div[1]/table/tbody/tr[3]/td[5]"));
 	datepicker1.click();
-	Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	driver.findElement(By.id("adults")).click();
-	Thread.sleep(2000);
+	
+	
+	
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	new Select(driver.findElement(By.id("adults"))).selectByVisibleText("4");
 	//driver.findElement(By.cssSelector("#adults > option[value=\"4\"]")).click();
 	driver.findElement(By.xpath("//button[@type='submit']")).click();
 	driver.findElement(By.xpath("//h4/a/b")).click();
 	for(int i=0;i<10;i++) {
 	driver.findElement(By.xpath(".//*[@id='OVERVIEW']/div/div[2]/div[1]/div[3]/div[2]/div/div[1]/div[3]")).click();
-	Thread.sleep(1000);
-}
-	driver.quit();
-}
-}
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+
+	
+}driver.quit();
+}}
 
